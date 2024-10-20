d515055e9c7953f38b08407a62fc7127d7b14792
emreeemul <emreemul20044@gmail.com>
2024-10-20 20:46:16 +0300

    Add files via upload

---
+++ iki sayının farkını veren c# kodu/iki sayının farkını veren c# kodu/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Birinci Sayıyı Giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Giriniz");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+int fark = sayi1 - sayi2;
+Console.WriteLine("Fark: " + fark);
+

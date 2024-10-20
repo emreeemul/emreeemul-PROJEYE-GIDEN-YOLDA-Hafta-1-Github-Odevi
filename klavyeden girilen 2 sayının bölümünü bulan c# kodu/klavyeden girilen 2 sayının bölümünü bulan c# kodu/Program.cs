125f411a35e3f4a87f51144fc76bf0d0452818fb
emreeemul <emreemul20044@gmail.com>
2024-10-20 20:50:52 +0300

    Add files via upload

---
+++ klavyeden girilen 2 sayının bölümünü bulan c# kodu/klavyeden girilen 2 sayının bölümünü bulan c# kodu/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Birinci Sayıyı Giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Giriniz");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+int bolum = sayi1 / sayi2;
+Console.WriteLine("Bölüm :  " + bolum);
+

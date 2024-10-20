d2374aceedff6a4561e681fe011bbc20c80815ef
emreeemul <emreemul20044@gmail.com>
2024-10-20 20:56:06 +0300

    Add files via upload

---
+++ 4 sayının toplamını ve çarpımını bulan c# kodu/klavyeden girilen 4 sayının toplamını ve çarpımını bulan c# kodu/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Birinci Sayıyı Giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Giriniz");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Üçüncü Sayıyı Giriniz");
+int sayi3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dördüncü Sayıyı Giriniz");
+int sayi4 = Convert.ToInt32(Console.ReadLine());
+int toplam = sayi1 + sayi2 + sayi3 + sayi4;
+int carpim = sayi1 * sayi2 * sayi3 * sayi4;
+Console.WriteLine("Toplam : " + toplam);
+Console.WriteLine("Çarpım : " + carpim);
+
+
---
+++ dikdörtgenler prizmasının hacminin c# kodu/kenarları girilen dikdörtgenler prizmasının hacminin c# kodu/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//Console.WriteLine("Birinci Kenarı Giriniz");
+//int kenar1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İkinci Kenarı Giriniz");
+//int kenar2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Yüksekliği Giriniz");
+//int yukseklik = Convert.ToInt32(Console.ReadLine());
+//int hacim = kenar1 * kenar2 * yukseklik;
+//Console.WriteLine("Hacim: " + hacim);
+
